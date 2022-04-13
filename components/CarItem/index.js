c7485042767046d353e1700780a3dcbaf79d4037
mahmoud_nourddine <mahmoudnourdein@gmail.com>
2022-04-13 21:53:40 +0200

    initial commit

---
+++ components/CarItem/index.js
@@ -0,0 +1,33 @@
+import { View, Text, ImageBackground, Image } from 'react-native'
+import React from 'react'
+import styles from './styles'
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome'
+import { faCog, faToolbox } from '@fortawesome/free-solid-svg-icons'
+
+
+export default function CarItem() {
+  return (
+    <View style={styles.carContainer}>
+      <ImageBackground 
+         source={require('../../assets/background.png')}
+         style={styles.backgroundImage}
+      />
+      {/* Header */}
+      <View style={styles.header}>
+          <FontAwesomeIcon style={styles.icon} icon={ faCog } size={24}/>
+          <Text style={styles.headerTitle}>NazMobile</Text>
+          <FontAwesomeIcon style={styles.icon} icon={ faToolbox } size={24}/>
+      </View>
+      <View style={styles.batterySection}>
+           <Image 
+             source={require('../../assets/battery.png')}
+             style={styles.batteryImage}
+           />
+           <Text style={styles.batteryText}>150 mi</Text>
+      </View>  
+      <View style={styles.status}>
+         <Text style={styles.statusText}>Parked</Text>
+      </View>   
+    </View>
+  )
+}
